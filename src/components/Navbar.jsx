918a545db3b018c46b0053f59e92d73df41e9211
123EFD <erchinqq06@gmail.com>
2025-08-08 23:06:50 +0800

    feat: update title in index.html and add Hero component with GSAP animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import { navLinks } from '../../constants'
-
+import gsap from 'gsap';
+import { navLinks } from '../../constants';
+import { useGSAP } from '@gsap/react';
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+        }
+    });
+
+    navTween.fromTo(
+        'nav',
+        { backgroundColor: 'transparent' },
+        {
+            backgroundColor: '#0000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut'
+        }
+    );
+});
+
   return (
     <nav>
         <div>
